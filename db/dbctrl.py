edfaefab15aab661eccd78c2e669a5ce63789733
Gabe Arnold <gabesarn@gmail.com>
2020-10-19 15:44:30 -0700

    Working on database controller.

---
+++ db/dbctrl.py
@@ -0,0 +1,34 @@
+import sqlite3
+import datetime
+from datetime import date
+import pandas as pd
+
+def db_details():
+    con = sqlite3.connect('news.db')
+    cursor = con.cursor()
+    info = cursor.execute('SELECT COUNT(titles) from news_titles')
+    info = info.fetchone()
+    return info
+
+def show_all_data():
+    con = sqlite3.connect('news.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT * from news_titles')
+    titles = cursor.fetchall()
+    con.close()
+    return titles
+
+def grab_titles():
+    title_list = []
+    dirty_data = pd.Series(show_headlines())
+    for i in dirty_data:
+        title_list.append(i[0])
+    return pd.Series(title_list)
+
+def show_headlines():
+    con = sqlite3.connect('news.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT article_title from news_titles')
+    titles = cursor.fetchall()
+    con.close()
+    return titles
