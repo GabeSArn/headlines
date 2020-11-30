14f5019e53f90e912ef01f7133eb040f2b0f4d52
Gabe Arnold <gabesarn@gmail.com>
2020-11-29 21:24:01 -0800

    Removed extra jupyter notebook. Cleaned main jupyter notebook. Updated scrapers formatting.

--- scrapers.py
+++ scrapers.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 def al_data_title_scraper():
-    al_data = urlopen('https://www.al.com/')
+    al_data = urlopen('www.al.com/')
     al_soup = BeautifulSoup(al_data, 'html.parser')
     articles = al_soup.find_all('article')
     regex_pattern = r'\d+[hm]\sago'
@@ -18,7 +18,7 @@ def al_data_title_scraper():
     return titles
 
 def fox_10_news_scraper():
-    fox_10_news = urlopen('https://www.fox10tv.com/')
+    fox_10_news = urlopen('www.fox10tv.com/')
     fox_10_news_soup = BeautifulSoup(fox_10_news, 'html.parser')
     fox_titles = fox_10_news_soup.find_all('a', class_='tnt-asset-link')
     remove_list = ['Tracking the Tropics', '1st & 10','COVID-19 Headlines', 'Back to School', 'News Now Update', 'Our Apps', 'News Now Livestream']
@@ -40,7 +40,7 @@ def fox_10_news_scraper():
     return list(set(final_fox_titles))
 
 def wbrc_scraper():
-    wbrc_birmingham = urlopen('https://www.wbrc.com/')
+    wbrc_birmingham = urlopen('www.wbrc.com/')
     wbrc_soup = BeautifulSoup(wbrc_birmingham, 'html.parser')
     new_titles = wbrc_soup.find_all('a',class_='unstyled-link')
     
@@ -52,7 +52,7 @@ def wbrc_scraper():
     return list(set(cleaned_titles))
 
 def al_birmingham_scraper():
-    al_birmingham = urlopen('https://www.al.com/birmingham/')
+    al_birmingham = urlopen('www.al.com/birmingham/')
     al_soup = BeautifulSoup(al_birmingham, 'html.parser')
     articles = al_soup.find_all('article')
 
@@ -69,7 +69,7 @@ def al_birmingham_scraper():
         return titles
     
 def whnt_scraper():
-    whnt = urlopen('https://www.whnt.com/')
+    whnt = urlopen('www.whnt.com/')
     whnt_soup = BeautifulSoup(whnt, 'html.parser')
     articles = whnt_soup.find_all(class_='article-list__article-title')
 
@@ -81,7 +81,7 @@ def whnt_scraper():
 
 def wsb_tv2_scraper():
 
-    wsb_tv2 = urlopen('https://www.wsbtv.com/')
+    wsb_tv2 = urlopen('www.wsbtv.com/')
     wsb_tv2_soup = BeautifulSoup(wsb_tv2,'html.parser')
     articles = wsb_tv2_soup.find_all(class_='unstyled-link')
 
@@ -94,7 +94,7 @@ def wsb_tv2_scraper():
 
 def nbc_dc_news_scraper():
     ### NBC Washington D.C.
-    wdc_nbc4 = urlopen('https://www.nbcwashington.com/')
+    wdc_nbc4 = urlopen('www.nbcwashington.com/')
     wdc_nbc4_soup = BeautifulSoup(wdc_nbc4, 'html.parser')
     articles = wdc_nbc4_soup.find_all(class_='story-card__title')
     titles = []
@@ -105,7 +105,7 @@ def nbc_dc_news_scraper():
     return titles
 
 def wdc_fox_scraper():
-    wdc_fox = urlopen('https://www.fox5dc.com/')
+    wdc_fox = urlopen('www.fox5dc.com/')
     wdc_soup = BeautifulSoup(wdc_fox, 'html.parser')
     articles = wdc_soup.find_all('article')
 
@@ -115,3 +115,4 @@ def wdc_fox_scraper():
         print(re.sub('\d\d\shours\sago','',i.text).rstrip().lstrip())
         
     return titles
+
