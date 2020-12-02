3f85c4c44f686e54d54fc581d669c5b94f4ec2c3
Gabe Arnold <gabesarn@gmail.com>
2020-12-02 12:54:04 -0800

    Debugged DC Scraper.

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from db import dbctrl as dbc
 
 def run_all():
     al_headlines_main = sc.al_data_title_scraper()
-    dbc.add_headlines(al_headlines_main, 'www.al.com')
+    dbc.add_headlines(al_headlines_main, 'https://www.al.com')
     fox_10_headlines = sc.fox_10_news_scraper()
-    dbc.add_headlines(fox_10_headlines, 'www.fox10tv.com')
+    dbc.add_headlines(fox_10_headlines, 'https://www.fox10tv.com')
     wbrc_headlines = sc.wbrc_scraper()
-    dbc.add_headlines(wbrc_headlines, 'www.wbrc.com/')
+    dbc.add_headlines(wbrc_headlines, 'https://www.wbrc.com/')
     birmingham_al = sc.al_birmingham_scraper()
     dbc.add_headlines(birmingham_al, 'https://www.al.com/birmingham/')
     huntsville_news = sc.whnt_scraper()
     dbc.add_headlines(huntsville_news, 'https://www.whnt.com/')
     wsb_tv2 = sc.wsb_tv2_scraper()
     dbc.add_headlines(wsb_tv2, 'https://www.wsbtv.com/') 
-    nbc_dc = nbc_dc_news_scraper()
+    nbc_dc = sc.nbc_dc_news_scraper()
     dbc.add_headlines(nbc_dc, 'https://www.nbcwashington.com/')    
-    wdc_fox = wdc_fox_scraper()
+    wdc_fox = sc.wdc_fox_scraper()
     dbc.add_headlines(wdc_fox, 'https://www.fox5dc.com/')
     
     return 'Program has sucessfully scraped and saved the data.'
